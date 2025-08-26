2d04802661e4d21cf0ef893a9ed1c7050586d454
shriyansh kasoundhan <shriyanshkasoudhan@gmail.com>
2025-08-26 19:13:53 +0530

    cors

--- src/main/java/com/example/dockerDemo/controller/CourseController.java
+++ src/main/java/com/example/dockerDemo/controller/CourseController.java
@@ -3,16 +3,12 @@ package com.example.dockerDemo.controller;
 import com.example.dockerDemo.entity.Course;
 import com.example.dockerDemo.service.CourseDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CourseController {
 
